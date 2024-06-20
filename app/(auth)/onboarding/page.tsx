20fbdcbfb353c947fb714f119eda5e75474f5b52
Navin grg <grgnabin60@gmail.com>
2024-06-20 18:23:10 +0545

    addded onboarding page and need to upgrade the node version to 22

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -24,7 +24,7 @@ async function Page() {
                 <section className="mt-9 bg-dark-2 p-10">
                     <AccountProfile 
                     user={userData}
-                    btnTitle= 
+                    btnTitle= ''
                     />
                 </section>
         </main>
